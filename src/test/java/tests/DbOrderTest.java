b04d64eab2cd748dfbb71936b748cba1ee641a6a
mrdcs92 <mrdcs92@gmail.com>
2026-08-26 19:06:17 -0700

    added prereq test for db tests

--- src/test/java/tests/DbOrderTest.java
+++ src/test/java/tests/DbOrderTest.java
@@ -1,6 +1,8 @@
 package tests;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
 
@@ -9,6 +11,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import components.BaseTest;
+import components.DatabaseUtils;
 import components.Retry;
 import data.DatabaseDataProvider;
 import pageobjects.CartPage;
@@ -19,7 +22,23 @@ import pageobjects.ProductCatalogue;
 
 public class DbOrderTest extends BaseTest {
 
-	@Test(dataProvider = "testinfodata", retryAnalyzer = Retry.class, dataProviderClass = DatabaseDataProvider.class)
+	@Test
+	public void verifyDatabaseConnection() {
+
+		try (Connection connection = DatabaseUtils.getConnection()) {
+
+			Assert.assertNotNull(connection, "Database connection should not be null");
+
+			Assert.assertFalse(connection.isClosed(), "Database connection should be open");
+
+		} catch (SQLException e) {
+
+			Assert.fail("Unable to connect to the QA database: " + e.getMessage(), e);
+		}
+	}
+
+	@Test(dataProvider = "testinfodata", dependsOnMethods = {
+			"verifyDatabaseConnection" }, retryAnalyzer = Retry.class, dataProviderClass = DatabaseDataProvider.class)
 	public void dbSubmitOrder(HashMap<String, String> input) throws IOException {
 
 		ProductCatalogue productCatalogue = landingPage.loginApplication(input.get("email"), input.get("password"));
